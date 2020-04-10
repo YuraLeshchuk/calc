fc3ee4f658c5ce64080fb81d9328accfc0f19644
YuraLeshchuk <57251419+YuraLeshchuk@users.noreply.github.com>
2020-04-10 14:58:46 +0300

    Prettier

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
-document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+document
+  .querySelectorAll(".digits button")
+  .forEach((button) => button.addEventListener("click", digitPressed));
 
 function digitPressed(ev) {
-   display.value += ev.target.innerText;
+  display.value += ev.target.innerText;
 }
 
-document.querySelectorAll('.opers button')
-    .forEach( button => button.addEventListener('click', opersPressed));
+document
+  .querySelectorAll(".opers button")
+  .forEach((button) => button.addEventListener("click", opersPressed));
 
 function opersPressed(ev) {
-   display.value += ev.target.innerText;
+  display.value += ev.target.innerText;
 }
 
-document.querySelector('.eq').addEventListener('click', calculate);
+document.querySelector(".eq").addEventListener("click", calculate);
 
 function calculate() {
-    display.value = eval.(display.value);
+  display.value = eval(display.value);
 }
 
-document.querySelector('.clear').addEventListener('click', reset);
+document.querySelector(".clear").addEventListener("click", reset);
 
 function reset() {
-   display.value = "";
+  display.value = "";
 }
 
-document.querySelector('.decimal').addEventListener('click', displayDecimal);
+document.querySelector(".decimal").addEventListener("click", displayDecimal);
 
 function displayDecimal() {
-    display.value = display.value + '.';
+  display.value = display.value + ".";
 }
-    
-document.querySelector('.back').addEventListener('click', deletLast);
+
+document.querySelector(".back").addEventListener("click", deletLast);
 
 function deletLast() {
-    const exp = display.value;
-    display.value = exp.substring(0, exp.length-1);
+  const exp = display.value;
+  display.value = exp.substring(0, exp.length - 1);
 }
